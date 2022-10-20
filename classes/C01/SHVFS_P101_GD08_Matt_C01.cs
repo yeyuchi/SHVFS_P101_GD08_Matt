a9bd238f688a976316aa442188c56b65074f3d6d
yeyuchi <1178171842@qq.com>
2022-10-20 22:08:21 +0800

    10.20

---
+++ classes/C01/SHVFS_P101_GD08_Matt_C01.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region Stracture of a program
+//Regions help you structure and organize your code
+
+//namespace
+//Contoin classes
+//Like a dictionary made of us,or by us,and provide commands and tools we can use
+//Note the syntax
+//Keyword/reserved word namespace followed by the name of our namespace,opening and closing curly braces
+namespace SHVFS_P101_GD08_Matt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int myFirstNumber = 5;
+            var mySecondNumber = 8;
+
+            Console.WriteLine($"my first number is:{myFirstNumber }");
+            Console.WriteLine($"My second numbr is:{mySecondNumber}");
+            Console.WriteLine();
+        }
+    }
+}
+#endregion
---
+++ classes/C04/SHVFS_P101_GD08_Matt_C04.cs
@@ -0,0 +1,103 @@
+﻿namespace SHVFS_P101_GD08_Matt_C04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //       var arithmetic0peratoratorMultiply = 0;
+            //     arithmetic0peratoratorMultiply=arithmetic0peratoratorMultiply+1;
+            //       Console.WriteLine(arithmetic0peratoratorMultiply);
+            //   Console.ReadLine();
+
+
+            //for loop
+            //first we assign a value to our variable,and declare one if it needs to be declared
+            //then there is an evaluation/check...
+            //then,we inrecment/decrement our variable at the end of the loop
+            //           Console.WriteLine("This is BEFORE our FOR LOOP!");
+            //        for(var i = 0; i <10; i++)
+            //           {
+            //               Console.WriteLine("This is BEFORE our FOR LOOP!");
+            //               Console.WriteLine($"The value pf is:{i}");
+            //          }
+            //          Console.WriteLine("This is AFTER our FOR LOOP!");
+            //        Console.ReadLine();
+
+            //while loop
+            //   var inpu = "";
+            //
+            ////
+            /////
+            ///while(input !="Quit")
+            //            {
+            //           Console.Clear();
+            //\can be used as an escape character,here let's use the litera
+            //           Console.WriteLine($"Enter \"Quit\" to win the game !");
+            //
+            //        }
+            //       Console.WriteLine("congrats!you win!");
+            //       Console.ReadLine();
+
+
+
+            //do while loop
+            //very siailar to while..
+
+
+
+            //       int i;
+            //           Console.WriteLine("Enter 1 for options\n" +
+            //               "2 to quit\n" +
+            //               "3 to start game");
+            //           do
+            //          {
+            //              Console.WriteLine("Please make a selection!HAHA1 ！");
+            //                  i=Convert.ToInt32(Console.ReadLine());
+
+            //          }
+            //          while (i<1||i>3);
+            //          Console.WriteLine("Excellent choice");
+            //          Console.ReadLine();
+            //nested for loops...
+            //you might work with these for 2D arrays,grids tiles...
+            //outer loop
+            //        for (var i = 0; i < 7; i++) 
+            //         {
+            //Inner Loop
+            //             for(var j = 0; j < 10; j++) 
+            //             {
+            //                 Console.WriteLine($"i is:{i},and j is:{j}");
+
+            //             }
+            //          }
+            //          Console.ReadLine();
+
+
+
+            //Arrays
+            //Arrays start at index ZERO
+            //Arrays hold STUFF/THINGS,and we set it to a size of FIVE(it can hold Five things!)
+            //This array holds ints!
+            //And we set it to hold
+  //          var scores = new int[5];
+    //        scores[0] = 7;
+      //      scores[1] = 24;
+        //    scores[2] = 2;
+ //           scores[3] = 9;
+   //         scores[4] = 1;
+     //       scores[5] = 23;
+       //     scores[6] = 99;
+       //
+            //this is bad
+            //it uses maguc numbers/hardcooed/inflexible/dangerous eg."5"
+         //   for(var i=0;i<5;i++)
+   //      for(var i = 0; i < scores.Length; i++)
+     //       {
+       //        // Console.WriteLine(scores[i]);
+         //      Console.WriteLine($"The length of scores is:{scores.Length}|The index is :{i}|The value is : {scores[i]}");
+       //     }
+    //        Console.ReadLine();
+
+        }
+    }
+}
